7cea737ed75c35bfdeacdebe7c133d37a0d3439a
Shailesh <mshaileshr@gmail.com>
2018-10-26 12:04:32 +0530

    Helper changed to realmPersistenceHelper

---
+++ app/src/main/java/com/contentstack/persistance/syncwrapper/SyncStore.java
@@ -0,0 +1,46 @@
+package com.contentstack.persistance.syncwrapper;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class SyncStore extends RealmObject {
+
+    @PrimaryKey
+    private String uniqueId;
+    private String sync_token;
+    private String pagination_token;
+
+
+    public SyncStore() { }
+
+
+    public SyncStore(String uniqueId, String sync_token, String pagination_token) {
+        this.uniqueId = uniqueId;
+        this.sync_token = sync_token;
+        this.pagination_token = pagination_token;
+    }
+
+    public String getUniqueId() {
+        return uniqueId;
+    }
+
+    public void setUniqueId(String uniqueId) {
+        this.uniqueId = uniqueId;
+    }
+
+    public String getSync_token() {
+        return sync_token;
+    }
+
+    public void setSync_token(String sync_token) {
+        this.sync_token = sync_token;
+    }
+
+    public String getPagination_token() {
+        return pagination_token;
+    }
+
+    public void setPagination_token(String pagination_token) {
+        this.pagination_token = pagination_token;
+    }
+}
